f394358e58b7d953819c436f299bffc0da601b9b
aminul <rahathssn@gmail.com>
2024-10-28 11:50:56 +0600

    added builders and routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+import notFound from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+
+// application routes
+app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Verv server is running");
 });
 
+app.use(globalErrorHandler as any);
 
+//Not Found
+app.use(notFound as any);
+
+export default app;
